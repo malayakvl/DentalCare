5d641ff35b2186d67b9735a2d43b974cc7d47b23
malayakvl <malaya.kvl@gmail.com>
2025-04-22 11:33:51 +0300

    Tooth35

--- resources/js/Pages/Patient/Formula/periodontit35/index.tsx
+++ resources/js/Pages/Patient/Formula/periodontit35/index.tsx
@@ -17,21 +17,21 @@ export default function PeriodontitStage35() {
                     opacity: (tooth35Diagnozis.periodontit_stage === 'st1'  ? 1 : 0),
                 }}
             >
-                <circle className="st42" cx="629.2" cy="1347.7" r="8.2" />
+                <circle className="st42" cx="1468.9" cy="1347.7" r="8.2" />
             </g>
             <g style={{
                     visibility: 'inherit',
                     opacity: (tooth35Diagnozis.periodontit_stage === 'st2'  ? 1 : 0)
             }}
             >
-                <circle className="st42" cx="627.4" cy="1354.5" r="17.5" />
+                <circle className="st42" cx="1470.7" cy="1354.5" r="17.5" />
             </g>
             <g style={{
                     visibility: 'inherit',
                     opacity: (tooth35Diagnozis.periodontit_stage === 'st3'  ? 1 : 0),
                 }}
             >
-                <circle className="st42" cx="625.2" cy="1367" r="30" />
+                <circle className="st42" cx="1472.9" cy="1367" r="30" />
             </g>
         </>
     )
