b8990dff5ed49a4d2ed69c6db4d5bb29ed4963b5
malayakvl <malaya.kvl@gmail.com>
2025-04-24 23:11:31 +0300

    Perio cart changes

--- resources/js/Pages/Patient/Perio/index.tsx
+++ resources/js/Pages/Patient/Perio/index.tsx
@@ -293,89 +293,144 @@ export default function Formula() {
                     <td></td>
                 </tr>
                 <tr>
-                    <td className="title"><span className="text-[14px]">Вестибулярно</span></td>
+                    <td className="title">Вестибулярно</td>
                     <td colSpan={16} style={{textAlign: 'center', width: '1024px'}}>
                         <FormulaPerio1828 />
                     </td>
                 </tr>
                 <tr>
-                    <td className="title">Глибина зонд-ння</td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
+                    <td className="title">Глибина зондування</td>
+                    <td><DeepZond toothNum={48} /></td>
+                    <td><DeepZond toothNum={47} /></td>
+                    <td><DeepZond toothNum={46} /></td>
+                    <td><DeepZond toothNum={45} /></td>
+                    <td><DeepZond toothNum={44} /></td>
+                    <td><DeepZond toothNum={43} /></td>
+                    <td><DeepZond toothNum={42} /></td>
+                    <td><DeepZond toothNum={41} /></td>
+                    <td><DeepZond toothNum={31} /></td>
+                    <td><DeepZond toothNum={32} /></td>
+                    <td><DeepZond toothNum={33} /></td>
+                    <td><DeepZond toothNum={34} /></td>
+                    <td><DeepZond toothNum={35} /></td>
+                    <td><DeepZond toothNum={36} /></td>
+                    <td><DeepZond toothNum={37} /></td>
+                    <td><DeepZond toothNum={38} /></td>
                 </tr>
                 <tr>
                     <td className="title">Ясений край</td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
+                    <td><DeepZond toothNum={48} /></td>
+                    <td><DeepZond toothNum={47} /></td>
+                    <td><DeepZond toothNum={46} /></td>
+                    <td><DeepZond toothNum={45} /></td>
+                    <td><DeepZond toothNum={44} /></td>
+                    <td><DeepZond toothNum={43} /></td>
+                    <td><DeepZond toothNum={42} /></td>
+                    <td><DeepZond toothNum={41} /></td>
+                    <td><DeepZond toothNum={31} /></td>
+                    <td><DeepZond toothNum={32} /></td>
+                    <td><DeepZond toothNum={33} /></td>
+                    <td><DeepZond toothNum={34} /></td>
+                    <td><DeepZond toothNum={35} /></td>
+                    <td><DeepZond toothNum={36} /></td>
+                    <td><DeepZond toothNum={37} /></td>
+                    <td><DeepZond toothNum={38} /></td>
                 </tr>
-                <tr>
-                    <td className="title">Зубні відкладення</td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
+                <tr style={{paddingTop: '10px'}}>
+                    <td style={{paddingTop: '10px'}} className="title">Зубні відкладення</td>
+                    <td style={{paddingTop: '10px'}}>
+                        <DentalDeposits toothNum={48} />
+                    </td>
+                    <td style={{paddingTop: '10px'}}>
+                        <DentalDeposits toothNum={47} />
+                    </td>
+                    <td style={{paddingTop: '10px'}}>
+                        <DentalDeposits toothNum={46} />
+                    </td>
+                    <td style={{paddingTop: '10px'}}>
+                        <DentalDeposits toothNum={45} />
+                    </td>
+                    <td style={{paddingTop: '10px'}}>
+                        <DentalDeposits toothNum={44} />
+                    </td>
+                    <td style={{paddingTop: '10px'}}>
+                        <DentalDeposits toothNum={43} />
+                    </td>
+                    <td style={{paddingTop: '10px'}}>
+                        <DentalDeposits toothNum={42} />
+                    </td>
+                    <td style={{paddingTop: '10px'}}>
+                        <DentalDeposits toothNum={41} />
+                    </td>
+                    <td style={{paddingTop: '10px'}}>
+                        <DentalDeposits toothNum={31} />
+                    </td>
+                    <td style={{paddingTop: '10px'}}>
+                        <DentalDeposits toothNum={32} />
+                    </td>
+                    <td style={{paddingTop: '10px'}}>
+                        <DentalDeposits toothNum={33} />
+                    </td>
+                    <td style={{paddingTop: '10px'}}>
+                        <DentalDeposits toothNum={34} />
+                    </td>
+                    <td style={{paddingTop: '10px'}}>
+                        <DentalDeposits toothNum={35} />
+                    </td>
+                    <td style={{paddingTop: '10px'}}>
+                        <DentalDeposits toothNum={36} />
+                    </td>
+                    <td style={{paddingTop: '10px'}}>
+                        <DentalDeposits toothNum={37} />
+                    </td>
+                    <td style={{paddingTop: '10px'}}>
+                        <DentalDeposits toothNum={38} />
+                    </td>
                 </tr>
                 <tr>
                     <td className="title">Гноєтеча</td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
+                    <td><Fertilizer toothNum={48} /></td>
+                    <td><Fertilizer toothNum={47} /></td>
+                    <td><Fertilizer toothNum={46} /></td>
+                    <td><Fertilizer toothNum={45} /></td>
+                    <td><Fertilizer toothNum={44} /></td>
+                    <td><Fertilizer toothNum={43} /></td>
+                    <td><Fertilizer toothNum={42} /></td>
+                    <td><Fertilizer toothNum={41} /></td>
+                    <td><Fertilizer toothNum={31} /></td>
+                    <td><Fertilizer toothNum={32} /></td>
+                    <td><Fertilizer toothNum={33} /></td>
+                    <td><Fertilizer toothNum={34} /></td>
+                    <td><Fertilizer toothNum={35} /></td>
+                    <td><Fertilizer toothNum={36} /></td>
+                    <td><Fertilizer toothNum={37} /></td>
+                    <td><Fertilizer toothNum={38} /></td>
                 </tr>
                 <tr>
                     <td className="title">Кровоточивість</td>
+                    <td><Bleeding toothNum={48} /></td>
+                    <td><Bleeding toothNum={47} /></td>
+                    <td><Bleeding toothNum={46} /></td>
+                    <td><Bleeding toothNum={45} /></td>
+                    <td><Bleeding toothNum={44} /></td>
+                    <td><Bleeding toothNum={43} /></td>
+                    <td><Bleeding toothNum={42} /></td>
+                    <td><Bleeding toothNum={41} /></td>
+                    <td><Bleeding toothNum={31} /></td>
+                    <td><Bleeding toothNum={32} /></td>
+                    <td><Bleeding toothNum={33} /></td>
+                    <td><Bleeding toothNum={34} /></td>
+                    <td><Bleeding toothNum={35} /></td>
+                    <td><Bleeding toothNum={36} /></td>
+                    <td><Bleeding toothNum={37} /></td>
+                    <td><Bleeding toothNum={38} /></td>
+                </tr>
+                
+                <tr>
+                    <td className="title">Фуркація</td>
+                    <td><Furkacia toothNum={48} /></td>
+                    <td><Furkacia toothNum={47} /></td>
+                    <td><Furkacia toothNum={46} /></td>
                     <td></td>
                     <td></td>
                     <td></td>
@@ -386,34 +441,31 @@ export default function Formula() {
                     <td></td>
                     <td></td>
                     <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
+                    <td><Furkacia toothNum={38} /></td>
+                    <td><Furkacia toothNum={38} /></td>
+                    <td><Furkacia toothNum={38} /></td>
                 </tr>
                 <tr>
-                    <td className="title">Фуркаці</td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
+                    <td className="title">Рухомість</td>
+                    <td><Moving toothNum={48} /></td>
+                    <td><Moving toothNum={47} /></td>
+                    <td><Moving toothNum={46} /></td>
+                    <td><Moving toothNum={45} /></td>
+                    <td><Moving toothNum={44} /></td>
+                    <td><Moving toothNum={43} /></td>
+                    <td><Moving toothNum={42} /></td>
+                    <td><Moving toothNum={41} /></td>
+                    <td><Moving toothNum={31} /></td>
+                    <td><Moving toothNum={32} /></td>
+                    <td><Moving toothNum={33} /></td>
+                    <td><Moving toothNum={34} /></td>
+                    <td><Moving toothNum={35} /></td>
+                    <td><Moving toothNum={36} /></td>
+                    <td><Moving toothNum={37} /></td>
+                    <td><Moving toothNum={38} /></td>
                 </tr>
                 <tr>
-                    <td className="title"><span className="text-[14px]">Рухомість</span></td>
+                    <td className="title">Рухомість</td>
                     <td colSpan={16} style={{textAlign: 'center', width: '1024px'}}>
                         <FormulaPerio3848 />
                     </td>
@@ -421,89 +473,89 @@ export default function Formula() {
                 <tr>
                     <td className="title">Глибина зондування</td>
                     <td><DeepZond toothNum={48} /></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
+                    <td><DeepZond toothNum={47} /></td>
+                    <td><DeepZond toothNum={46} /></td>
+                    <td><DeepZond toothNum={45} /></td>
+                    <td><DeepZond toothNum={44} /></td>
+                    <td><DeepZond toothNum={43} /></td>
+                    <td><DeepZond toothNum={42} /></td>
+                    <td><DeepZond toothNum={41} /></td>
+                    <td><DeepZond toothNum={31} /></td>
+                    <td><DeepZond toothNum={32} /></td>
+                    <td><DeepZond toothNum={33} /></td>
+                    <td><DeepZond toothNum={34} /></td>
+                    <td><DeepZond toothNum={35} /></td>
+                    <td><DeepZond toothNum={36} /></td>
+                    <td><DeepZond toothNum={37} /></td>
+                    <td><DeepZond toothNum={38} /></td>
                 </tr>
                 <tr>
                     <td className="title">Ясений край</td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
+                    <td><DeepZond toothNum={48} /></td>
+                    <td><DeepZond toothNum={47} /></td>
+                    <td><DeepZond toothNum={46} /></td>
+                    <td><DeepZond toothNum={45} /></td>
+                    <td><DeepZond toothNum={44} /></td>
+                    <td><DeepZond toothNum={43} /></td>
+                    <td><DeepZond toothNum={42} /></td>
+                    <td><DeepZond toothNum={41} /></td>
+                    <td><DeepZond toothNum={31} /></td>
+                    <td><DeepZond toothNum={32} /></td>
+                    <td><DeepZond toothNum={33} /></td>
+                    <td><DeepZond toothNum={34} /></td>
+                    <td><DeepZond toothNum={35} /></td>
+                    <td><DeepZond toothNum={36} /></td>
+                    <td><DeepZond toothNum={37} /></td>
+                    <td><DeepZond toothNum={38} /></td>
                 </tr>
-                <tr>
-                    <td className="title">Зубні відкладення</td>
-                    <td>
+                <tr style={{paddingTop: '10px'}}>
+                    <td style={{paddingTop: '10px'}} className="title">Зубні відкладення</td>
+                    <td style={{paddingTop: '10px'}}>
                         <DentalDeposits toothNum={48} />
                     </td>
-                    <td>
+                    <td style={{paddingTop: '10px'}}>
                         <DentalDeposits toothNum={47} />
                     </td>
-                    <td>
+                    <td style={{paddingTop: '10px'}}>
                         <DentalDeposits toothNum={46} />
                     </td>
-                    <td>
+                    <td style={{paddingTop: '10px'}}>
                         <DentalDeposits toothNum={45} />
                     </td>
-                    <td>
+                    <td style={{paddingTop: '10px'}}>
                         <DentalDeposits toothNum={44} />
                     </td>
-                    <td>
+                    <td style={{paddingTop: '10px'}}>
                         <DentalDeposits toothNum={43} />
                     </td>
-                    <td>
+                    <td style={{paddingTop: '10px'}}>
                         <DentalDeposits toothNum={42} />
                     </td>
-                    <td>
+                    <td style={{paddingTop: '10px'}}>
                         <DentalDeposits toothNum={41} />
                     </td>
-                    <td>
+                    <td style={{paddingTop: '10px'}}>
                         <DentalDeposits toothNum={31} />
                     </td>
-                    <td>
+                    <td style={{paddingTop: '10px'}}>
                         <DentalDeposits toothNum={32} />
                     </td>
-                    <td>
+                    <td style={{paddingTop: '10px'}}>
                         <DentalDeposits toothNum={33} />
                     </td>
-                    <td>
+                    <td style={{paddingTop: '10px'}}>
                         <DentalDeposits toothNum={34} />
                     </td>
-                    <td>
+                    <td style={{paddingTop: '10px'}}>
                         <DentalDeposits toothNum={35} />
                     </td>
-                    <td>
+                    <td style={{paddingTop: '10px'}}>
                         <DentalDeposits toothNum={36} />
                     </td>
-                    <td>
+                    <td style={{paddingTop: '10px'}}>
                         <DentalDeposits toothNum={37} />
                     </td>
-                    <td>
+                    <td style={{paddingTop: '10px'}}>
                         <DentalDeposits toothNum={38} />
                     </td>
                 </tr>
